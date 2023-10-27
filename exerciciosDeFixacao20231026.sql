ea7f2cd0535876b9ce73bd07f1b3c5b5999746fc
BrunoH300 <116306479+BrunoH300@users.noreply.github.com>
2023-10-26 21:14:07 -0300

    Update exerciciosDeFixacao20231026.sql
    Atividade, fim

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -5,3 +5,24 @@ BEGIN
     INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Mensagem inserida as:', NOW()));
 END;
 
+
+CREATE TRIGGER before_delete_client
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Tentativas de excluir: ', OLD.nome));
+END;
+
+
+CREATE TRIGGER after_update_client
+AFTER UPDATE ON Clientes
+FOR EACH ROW
+BEGIN
+INSERT INTO Auditoria (mensagem) VALUES ('Tentativas de atualização: ');
+    ELSE
+INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Nome do cliente atualizado: "', OLD.nome, '" para "', NEW.nome, '"'));
+    END IF;
+END;
+
+
+
